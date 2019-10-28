25c834ec41df8fda4d6a48f8a94707b5cc5c5cf6
Niklas <46811902+2Who@users.noreply.github.com>
2019-10-28 13:37:25 +0100

    bla
    bla

--- sysprog_01.X/newmainXC16.c
+++ sysprog_01.X/newmainXC16.c
@@ -5,9 +5,28 @@
  * Created on 15. Oktober 2019, 15:25
  */
 
+#include <xc.h>
+#define    FCY    16000000UL // Instruction cycle frequency, Hz - required for __delayXXX() to work
+#include <libpic30.h>        // __delayXXX() functions macros defined here
+#include "mcc_generated_files/mcc.h"
 
-#include "xc.h"
 
 int main(void) {
+        TRISA = 0x0;
+        TRISB = 0x0;
+        TRISC = 0x0;
+    while (1)
+    {
+        // Add your application code
+        
+        adc_result_t dutyValue = ADCC_GetSingleConversion(pot); //load the potentiometer value in a 16 bit variable
+                      
+        if (dutyValue < 90) //if the potentiometer does not give 0 value
+            dutyValue = 0; //set the value to 0 when it is below 90 to turn the LED off completely
+        
+        PWM6_LoadDutyValue(dutyValue); //lod the potentiometer value to the PWM duty cycle
+        
+                      
+    }
     return 0;
 }
